d174634a1b7e3104bddca31c772f4e3afc763768
christo667 <123850493+christo667@users.noreply.github.com>
2024-03-05 13:23:10 -0600

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,54 @@
+/*
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * @author  Michael Kolling and David J. Barnes
+ * @version 1.0 (February 2002)
+ */
+
+public class Room 
+{
+    public String description;
+    public Room northExit;
+    public Room southExit;
+    public Room eastExit;
+    public Room westExit;
+
+    /**
+     * Create a room described "description". Initially, it has
+     * no exits. "description" is something like "a kitchen" or
+     * "an open court yard".
+     */
+    public Room(String description) 
+    {
+        this.description = description;
+    }
+
+    /**
+     * Define the exits of this room.  Every direction either leads
+     * to another room or is null (no exit there).
+     */
+    public void setExits(Room north, Room east, Room south, Room west) 
+    {
+        if(north != null)
+            northExit = north;
+        if(east != null)
+            eastExit = east;
+        if(south != null)
+            southExit = south;
+        if(west != null)
+            westExit = west;
+    }
+
+    /**
+     * Return the description of the room (the one that was defined
+     * in the constructor).
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+}
